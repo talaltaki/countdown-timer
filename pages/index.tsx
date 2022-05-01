8045e8934d86ba53dc031b4cc81f4f7b81076f41
Talal Taki <talaltaki.tt@gmail.com>
2022-05-02 00:00:27 +0600

    Made some changes to index.tsx, globals.css & tailwind.config.js files

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center bg-[#1e1f29]">
       <Head>
-        <title>Launch Countdown Timer</title>
+        <title>Countdown Timer</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
     </div>
